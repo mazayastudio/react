af40492631e966702b049ae635503cd08608bd02
sayudha <mazaya.studio@outlook.com>
2024-12-04 07:13:01 +0700

    Add close button to toggle step navigation visibility

--- 2-steps/src/App.js
+++ 2-steps/src/App.js
@@ -3,6 +3,7 @@ const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new 
 
 export default function App() {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
 		if (step > 1) setStep(step - 1);
@@ -13,25 +14,32 @@ export default function App() {
 	}
 
 	return (
-		<div className='steps'>
-			<div className='numbers'>
-				<div className={step >= 1 ? 'active' : ''}>1</div>
-				<div className={step >= 2 ? 'active' : ''}>2</div>
-				<div className={step >= 3 ? 'active' : ''}>3</div>
-			</div>
+		<>
+			<button className='close' onClick={() => setIsOpen(!isOpen)}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className='steps'>
+					<div className='numbers'>
+						<div className={step >= 1 ? 'active' : ''}>1</div>
+						<div className={step >= 2 ? 'active' : ''}>2</div>
+						<div className={step >= 3 ? 'active' : ''}>3</div>
+					</div>
 
-			<p className='message'>
-				Step {step}: {messages[step - 1]}
-			</p>
+					<p className='message'>
+						Step {step}: {messages[step - 1]}
+					</p>
 
-			<div className='buttons'>
-				<button style={{ backgroundColor: '#7950f2', color: '#ffffff' }} onClick={handlePrevious}>
-					Previous
-				</button>
-				<button style={{ backgroundColor: '#7950f2', color: '#ffffff' }} onClick={handleNext}>
-					Next
-				</button>
-			</div>
-		</div>
+					<div className='buttons'>
+						<button style={{ backgroundColor: '#7950f2', color: '#ffffff' }} onClick={handlePrevious}>
+							Previous
+						</button>
+						<button style={{ backgroundColor: '#7950f2', color: '#ffffff' }} onClick={handleNext}>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
